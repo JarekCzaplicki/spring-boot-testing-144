a86422a43bb49b2ed7f2e703ac3c7e2f3d9e298f
jarekczaplicki <jarekczaplicki@tlen.pl>
2023-12-14 18:36:34 +0100

    Initialization of project

---
+++ src/test/java/org/example/springboottesting144/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,25 @@
+package org.example.springboottesting144.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+//@SpringBootTest
+class EmployeeRepositoryTest {
+// testy do zapisywania pracowników
+    // pobierania wszystkich pracowników
+    // pobieranie pracownika po 'Id'
+    // pobieranie pracownika po 'email'
+    // uaktualnianie danych pracownika
+    // usuwanie pracownika
+    // testy dla zapytań JPQL
+    // testy dla zapytań z indeksowanymi parametrami
+    // testy dla zapytań z parametryzowanymi parametrami
+    // Piszemy w TDD oraz BDD
+    // BDD
+    //given - aktualne ustawienia , obiekty
+    //when - akcja, metoda, zachowanie które testujemy
+    //then - weryfikacja rezultatu
+
+}
