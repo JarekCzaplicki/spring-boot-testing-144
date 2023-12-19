ae61c44b1ec2e0511157a76377d01c4b0cc1fbeb
jarekczaplicki <jarekczaplicki@tlen.pl>
2023-12-19 19:12:48 +0100

    Unit testing for Controller layer

--- src/main/java/org/example/springboottesting144/controller/EmployeeController.java
+++ src/main/java/org/example/springboottesting144/controller/EmployeeController.java
@@ -3,7 +3,10 @@ package org.example.springboottesting144.controller;
 import org.example.springboottesting144.model.Employee;
 import org.example.springboottesting144.service.EmployeeService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -14,10 +17,36 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-//    @RequestMapping(method = RequestMethod.POST);
+    //    @RequestMapping(method = RequestMethod.POST);
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee createEmployee(@RequestBody Employee employee){
+    public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
+        return employeeService.getEmployeeById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployeeById(@PathVariable long id,
+                                                       @RequestBody Employee employee) {
+        return employeeService.getEmployeeById(id)
+                .map(savedEmployee -> {
+                            savedEmployee.setFirstName(employee.getFirstName());
+                            savedEmployee.setLastName(employee.getLastName());
+                            savedEmployee.setEmail(employee.getEmail());
+                            Employee updatedEmployee = employeeService.updateEmployee(savedEmployee);
+                            return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
+                        })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
